ab0b852fa528481ae36b846ad4dc3c0ac63b6320
Alexey Chistov <alk93@rambler.ru>
2024-02-04 09:21:10 +0300

    del trash

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ VK_GROUP_ID = os.getenv('GROUP_ID')
 REPEAT_TIME = int(os.getenv('REPEAT_SECONDS'))
 
 
-def run2():
+def run():
     vk_session = vk_api.VkApi(token=VK_API_TOKEN)
     vk = vk_session.get_api()
 
@@ -35,36 +35,5 @@ def run2():
         time.sleep(REPEAT_TIME)
 
 
-def run1():
-    vk_session = vk_api.VkApi(token=TOKEN)
-    vk = vk_session.get_api()
-
-    last_checked_id = 0
-    while True:
-        time.sleep(REPEAT_TIME)
-        print(f'last_checked_id={last_checked_id}')
-        get_response = vk.wall.get(owner_id=f'-{VK_GROUP_ID}', offset=0)
-        count = get_response['count']
-        print(f'count={count}')
-        if count == 0:
-            continue
-        actual_id = get_response['items'][0]['id']
-        print(f'actual_id={actual_id}')
-        if actual_id <= last_checked_id:
-            continue
-        items = list(map(lambda item: (item['id'], item['text']), get_response['items']))
-        for it in items:
-            print(f'check id={it[0]} text={it[1]}')
-            if it[0] > last_checked_id:
-                if it[1] == 'banString':
-                    print(f'DELETE! id={it[0]}')
-            else:
-                break
-        last_checked_id = actual_id
-        print(items)
-
-
 if __name__ == '__main__':
-    run2()
-
-# https://oauth.vk.com/authorize?client_id=51848136&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=wall,groups,offline&response_type=token&v=5.199
+    run()
